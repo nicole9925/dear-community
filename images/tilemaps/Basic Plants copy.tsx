d2837b2f2d30cfb30f9b4c7047224f5e67ea5272
Nicole <nicolemandylee8@gmail.com>
2022-02-06 03:01:44 -0800

    change links

--- images/tilemaps/Basic Plants copy.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Basic Plants copy" tilewidth="1" tileheight="1" tilecount="1200" columns="60">
- <image source="images/Sprout Lands - Sprites - Basic pack/Objects/Basic Plants copy.png" trans="ff00ff" width="60" height="20"/>
-</tileset>
