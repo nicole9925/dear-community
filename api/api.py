8033929ccf4086fbfe49fe9978177f53635a02df
Nawach Kuptimanus <kuptiman@Nawachs-MacBook-Air.local>
2022-02-05 18:31:44 -0800

    for oscar test

--- api/api.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api, Resource, reqparse
-
-class ApiHandler(Resource):
-  def get(self):
-    return {
-      'resultStatus': 'SUCCESS',
-      'message': "hello from api"
-      }
